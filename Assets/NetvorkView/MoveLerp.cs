6812b1ad8151242979fa664b17a9cadd5548ffc5
moniakk <nroshkius@gmail.com>
2015-10-16 20:40:47 +0400

    tcp

--- Assets/NetvorkView/MoveLerp.cs
+++ Assets/NetvorkView/MoveLerp.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveLerp : MonoBehaviour
-{
+public class MoveLerp : MonoBehaviour {
 
     public Vector3 position = Vector3.zero;
     public Quaternion rotation = Quaternion.identity;
 
     public float offset = 0;
-    public bool IsMine = false;
+    public bool IsMine = true;
 
-    void Update()
-    {
-        if (!IsMine)
-        {
+    void Update() {
+        if (!IsMine) {
             offset += Time.deltaTime;
-            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, position, offset );
+            gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, position, offset);
         }
     }
 }
--- Assets/NetvorkView/NetworkCore.cs
+++ Assets/NetvorkView/NetworkCore.cs
@@ -7,14 +7,14 @@ using System.Collections.Generic;
 
 
 
-public class NetworkCore : MonoBehaviour
-{
+public class NetworkCore : MonoBehaviour {
     public GameObject player;
-    public static List<NetworkGameObject> ObjectReference = new List<NetworkGameObject>();
+    //   public static List<NetworkGameObject> ObjectReference = new List<NetworkGameObject>();
+    public static List<NetworkGameObject2D> ObjectReference2D = new List<NetworkGameObject2D>();
+
     public GameObject obj;
     static Socket client;
-    public static void Connect(string ip, int port)
-    {
+    public static void Connect(string ip, int port) {
         client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
         client.Connect(ip, port);
         getAllObject();
@@ -24,22 +24,19 @@ public class NetworkCore : MonoBehaviour
     }
 
 
-    void received()
-    {
+    void received() {
         bool receive_Size = true;
         byte[] buffer = new byte[1];
         int bytesRec = 0;
 
-        if (receive_Size && client.Available >= 1)
-        {
+        if (receive_Size && client.Available >= 1) {
             receive_Size = false;
             buffer = new byte[1];
             client.Receive(buffer, buffer.Length, SocketFlags.None);
             bytesRec = buffer[0];
         }
 
-        if (!receive_Size && client.Available >= bytesRec)
-        {
+        if (!receive_Size && client.Available >= bytesRec) {
             receive_Size = true;
             buffer = new byte[bytesRec];
             client.Receive(buffer, buffer.Length, SocketFlags.None);
@@ -52,8 +49,7 @@ public class NetworkCore : MonoBehaviour
         }
     }
 
-    private static void getAllObject()
-    {
+    private static void getAllObject() {
 
         byte[] length = new byte[] { (byte)1 };
         byte[] code = new byte[] { (byte)3 };
@@ -61,8 +57,16 @@ public class NetworkCore : MonoBehaviour
         client.Send(length);
 
     }
-    private static void Send(NetworkGameObject obj, int Code)
-    {
+    private static void Send(NetworkGameObject2D obj, int Code) {
+        byte[] data = obj.ToByte();
+        byte[] length = new byte[] { (byte)(data.Length + 1) };
+        byte[] code = new byte[] { (byte)Code };
+        length = length.AddByte(code);
+        length = length.AddByte(data);
+        client.Send(length);
+
+    }
+    private static void Send(string obj, int Code) {
         byte[] data = obj.ToByte();
         byte[] length = new byte[] { (byte)(data.Length + 1) };
         byte[] code = new byte[] { (byte)Code };
@@ -72,21 +76,20 @@ public class NetworkCore : MonoBehaviour
 
     }
 
-    public static void Instantiates(GameObject Gameobj)
-    {
+    public static void Instantiates(GameObject Gameobj) {
 
         if (!client.Connected)
             return;
 
 
-        NetworkGameObject obj = new NetworkGameObject();
+        NetworkGameObject2D obj = new NetworkGameObject2D();
         obj.gameObject = Instantiate(Gameobj);
         obj.gameObject.name = "test/Player1";
         obj.IsMine = true;
 
-        obj.moveLerp = obj.gameObject.GetComponent<MoveLerp>();
-        obj.moveLerp.IsMine = true;
-        ObjectReference.Add(obj);
+        //obj.moveLerp = obj.gameObject.GetComponent<MoveLerp2D>();
+        // obj.moveLerp.IsMine = true;
+        ObjectReference2D.Add(obj);
         Camera.main.GetComponent<CmaeraRunnerScript>().Player = obj.gameObject.transform;
 
         byte[] data = obj.ToByte(obj.gameObject.GetInstanceID());
@@ -99,47 +102,44 @@ public class NetworkCore : MonoBehaviour
     }
 
 
-    void setID(byte[] byteArray)
-    {
+    void setID(byte[] byteArray) {
         int currentID = byteArray.Take(4).ToArray().ToInt();
         int NewId = byteArray.Skip(4).Take(4).ToArray().ToInt();
-        ObjectReference.First(x => x.gameObject.GetInstanceID() == currentID).ID = NewId;
-
+        ObjectReference2D.First(x => x.gameObject.GetInstanceID() == currentID).ID = NewId;
     }
 
-    void newObject(byte[] buffer)
-    {
-        NetworkGameObject result = buffer.NewGameObject();
+    void newObject(byte[] buffer) {
+        NetworkGameObject2D result = buffer.NewGameObject2D();
         result.gameObject = (GameObject)Instantiate(result.gameObject, result.position, result.rotation);
-        result.moveLerp = result.gameObject.GetComponent<MoveLerp>();
-        ObjectReference.Add(result);
+        MoveLerp2D moveLerp2D = new MoveLerp2D();
+        result.moveLerp = result.gameObject.gameObject.AddComponent<MoveLerp2D>();
+        //  result.moveLerp = result.gameObject.GetComponent<MoveLerp2D>();
+        ObjectReference2D.Add(result);
         result.gameObject.GetComponent<UnityStandardAssets._2D.Platformer2DUserControl>().enabled = false;
-       
-
-
     }
 
-    void work(int Code, byte[] buffer)
-    {
-        if (Code == 2)
-        {
+    void work(int Code, byte[] buffer) {
+        if (Code == 2) {
             setID(buffer);
         }
-        if (Code == 3)
-        {
+        if (Code == 3) {
             newObject(buffer);
         }
-        if (Code == 4)
-        {
+        if (Code == 4) {
             update(buffer);
+        }
+        if (Code == 5) {
+            NetworkDestroyObject(buffer);
+        }
+        if (Code == 255) {
+            Send("", 255);
         }
 
     }
 
-    void update(byte[] byteArray)
-    {
-        NetworkGameObject ReceiveObject = byteArray.GetGameObject();
-        var target = ObjectReference.First(x => x.ID == ReceiveObject.ID);
+    void update(byte[] byteArray) {
+        NetworkGameObject2D ReceiveObject = byteArray.GetGameObject2D();
+        var target = ObjectReference2D.First(x => x.ID == ReceiveObject.ID);
         target.position = ReceiveObject.position;
         target.rotation = ReceiveObject.rotation;
         target.moveLerp.position = ReceiveObject.position;
@@ -148,19 +148,15 @@ public class NetworkCore : MonoBehaviour
 
     }
 
-    void Start()
-    {
+    void Start() {
         //Instantiates(player);
     }
 
     float time;
-    public void SyncObject()
-    {
-        if ((Time.time - time) > 0.1f)
-        {
+    public void SyncObject() {
+        if ((Time.time - time) > 0.1f) {
             time = Time.time;
-            foreach (var item in ObjectReference.Where(x => x.ID != 0 && x.IsMine && (x.gameObject.transform.position != x.position || x.gameObject.transform.rotation != x.rotation)))
-            {
+            foreach (var item in ObjectReference2D.Where(x => x.ID != 0 && x.IsMine && ((Vector2)x.gameObject.transform.position != x.position || x.gameObject.transform.rotation != x.rotation || (Time.time - time) > 2))) {
                 item.position = item.gameObject.transform.position;
                 item.rotation = item.gameObject.transform.rotation;
                 Send(item, 2);//update
@@ -168,16 +164,17 @@ public class NetworkCore : MonoBehaviour
         }
     }
 
-    void Update()
-    {
-        if (client != null && client.Connected)
-        {
+    void Update() {
+        if (client != null && client.Connected) {
             received();
             SyncObject();
-
         }
-
     }
 
+    void NetworkDestroyObject(byte[] byteArray) {
+        var destroyObject = ObjectReference2D.First(x => x.ID == byteArray.ToInt());
+        ObjectReference2D.Remove(destroyObject);
+        Destroy(destroyObject.gameObject);
 
+    }
 }
