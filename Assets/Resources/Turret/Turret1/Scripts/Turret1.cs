44cc07b6e2828c758a6abef74406680302ac407d
moniakk <nroshkius@gmail.com>
2015-11-03 23:06:05 +0400

    turret1
    turret1

--- Assets/Resources/Turret/Turret1/Scripts/Turret1.cs
+++ Assets/Resources/Turret/Turret1/Scripts/Turret1.cs
@@ -4,41 +4,59 @@ using System.Collections;
 public class Turret1 : MonoBehaviour {
 
     public float LockSpeed = 1f;
-    public float DoorLock = 0f;
+    public float ShootSpeed = 1f;
     Transform head;
-    Transform doorLeft;
-    Transform doorRight;
     GameObject targetObject;
+    Animator anim;
+    public GameObject bullet;
+    float ShootLastTime = 0;
     bool IsStarted;
     bool ReadyToShoot;
+    DoorStatus DoorInfo;
     void Start() {
-        head = transform.FindChild("turretHead");
-        doorLeft = transform.FindChild("doorLeft");
-        doorRight = transform.FindChild("doorRight");
+        anim = GetComponent<Animator>();
+        head = transform.FindChild("Head");
+      
     }
 
 
     void Update() {
-        if (IsStarted) {
-            doorLeft.localPosition = new Vector2(DoorLock, 0);
-            doorRight.localPosition = new Vector2(-DoorLock, 0);
-
-            doorLeft.localScale = new Vector3(3 - DoorLock * 10, 5.37f, 1);
-            doorRight.localScale = new Vector3(3 - DoorLock * 10, 5.37f, 1);
-
-
-            head.localScale = new Vector3(1 + DoorLock, 1 + DoorLock, 1 + DoorLock);
-
+        if (DoorInfo == DoorStatus.IsClosed) {
+            HeadRotation(Quaternion.identity);
         }
-        if (ReadyToShoot) {
+
+        if (IsStarted && DoorInfo == DoorStatus.IsOpened) {
             var dir = targetObject.transform.position - transform.position;
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             var newRotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
             //  head.rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
-            head.rotation = Quaternion.Slerp(head.rotation, newRotation, Time.deltaTime * LockSpeed);
-            Vector3 v3 = head.TransformDirection(Vector3.up) * 10;
-            Debug.DrawRay(head.position, head.up * 100, Color.blue);
+            HeadRotation(newRotation);
+        }
 
+
+    }
+    void shoot() {
+
+    }
+
+    void DoorInfoStatus(DoorStatus status) {
+        DoorInfo = status;
+    }
+
+
+    void HeadRotation(Quaternion newRotation) {
+        head.rotation = Quaternion.Slerp(head.rotation, newRotation, Time.deltaTime * LockSpeed);
+        Vector3 v3 = head.TransformDirection(Vector3.up) * 10;
+        Debug.DrawRay(head.position, head.up * 100, Color.blue);
+        Debug.DrawLine(head.position + head.up,  head.up*100, Color.red);
+        RaycastHit2D hit= Physics2D.Raycast(head.position + head.transform.up, head.up * 100, 100,(1<<9));
+        if (hit.collider != null && hit.transform.tag == "Player") {
+            Debug.Log("dasda");
+            if (bullet!=null && (Time.fixedTime-ShootLastTime)> ShootSpeed) {
+                ShootLastTime = Time.fixedTime;
+            var currentBullet=    Instantiate(bullet, head.position + head.up, head.rotation);
+                Destroy(currentBullet, 5);
+            }
         }
 
     }
@@ -46,16 +64,23 @@ public class Turret1 : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collaider) {
         if (collaider.tag == "Player") {
+            anim.SetBool("Open", true);
             targetObject = collaider.gameObject;
             IsStarted = true;
         }
-
     }
-    void OnTriggerLeave2D(Collider2D collaider) {
+    void OnTriggerExit2D(Collider2D collaider) {
         if (collaider.tag == "Player") {
+            anim.SetBool("Open", false);
             targetObject = null;
             IsStarted = false;
         }
     }
 
+}
+enum DoorStatus {
+    IsOpened = 1,//xia
+    IsClosed = 2,//daketilia
+    //IsOpenening = 4,//ixsneba
+    //IsClosing = 3//iketeba
 }
---
+++ Assets/Resources/Turret/bullets/Scripts/bullet1.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Turret {
+
+    public class bullet1 : MonoBehaviour {
+
+     
+        void Start() {
+
+        }
+
+       
+        void Update() {
+            transform.Translate(0, 0.03f, 0);
+        }
+    }
+}
+
