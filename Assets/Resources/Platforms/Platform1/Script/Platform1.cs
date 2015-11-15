b195b17801e7d0501276bff6dc110c14d17f83fc
moniakk <nroshkius@gmail.com>
2015-11-16 01:33:40 +0400

    Water

---
+++ Assets/Resources/Platforms/Platform1/Script/Platform1.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Platform1 : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    SpriteRenderer sprite;
+    void Awake()
+    {
+        //var body = transform.FindChild("Body");
+
+        //sprite = body.GetComponent<SpriteRenderer>();
+
+        //Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+
+        //GameObject childPrefab = new GameObject();
+
+        //SpriteRenderer childSprite = childPrefab.AddComponent<SpriteRenderer>();
+        //childPrefab.transform.position = transform.position;
+        //childSprite.sprite = sprite.sprite;
+
+        //GameObject child;
+        //for (int i = 0, h = (int)Mathf.Round(sprite.bounds.size.y); i * spriteSize.y < h; i++)
+        //{
+        //    for (int j = 0, w = (int)Mathf.Round(sprite.bounds.size.x); j * spriteSize.x < w; j++)
+        //    {
+        //        child = Instantiate(childPrefab) as GameObject;
+        //        child.transform.position = transform.position - (new Vector3(spriteSize.x * j, spriteSize.y * i, 0));
+        //        child.transform.parent = transform;
+        //    }
+        //}
+
+        //Destroy(childPrefab);
+        //sprite.enabled = false;
+
+    }
+
+
+
+    void OnDrawGizmos()
+    {
+
+        var body = transform.FindChild("Body");
+
+        int childs = transform.childCount;
+        for (int i = 0; i < childs; i++)
+        {
+            GameObject.Destroy(transform.GetChild(i).gameObject);
+        }
+
+
+      
+
+
+
+
+        return;
+        sprite = body.GetComponent<SpriteRenderer>();
+
+        Vector2 spriteSize = new Vector2(sprite.bounds.size.x / transform.localScale.x, sprite.bounds.size.y / transform.localScale.y);
+
+
+
+        Transform child;
+        for (int i = 0, h = (int)Mathf.Round(sprite.bounds.size.y); i * spriteSize.y < h; i++)
+        {
+            for (int j = 0, w = (int)Mathf.Round(sprite.bounds.size.x); j * spriteSize.x < w; j++)
+            {
+                child = Instantiate(body) as Transform;
+                child.position = transform.position - (new Vector3(spriteSize.x * j, spriteSize.y * i, 0));
+                child.parent = transform;
+            }
+        }
+
+        // Destroy(childPrefab);
+        //sprite.enabled = false;
+
+
+    }
+}
---
+++ Assets/Resources/Water/Water2/Script/Water2.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Water2 : MonoBehaviour
+{
+
+    Renderer render;
+    Mesh mesh;
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        render = GetComponent<Renderer>();
+        if (render.material != null)
+        {
+            //render.material.mainTextureScale = new Vector2(transform.localScale.x / transform.localScale.y, render.material.mainTextureScale.y);
+            render.material.mainTextureScale = new Vector2(transform.localScale.x / transform.localScale.y, render.material.mainTextureScale.y);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3[] verticles = mesh.vertices;
+        //for (int i = 0; i < mesh.vertexCount; i++)
+        //{
+        //    verticles[i] =new Vector3(Random.Range(0, 2), Random.Range(0, 2), Random.Range(0, 2));
+        //}
+        //mesh.vertices = verticles;
+        //mesh.RecalculateBounds();
+        //mesh.RecalculateNormals();
+        //GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+
+    void OnDrawGizmos()
+    {
+        render = GetComponent<Renderer>();
+
+        if (render.material != null)
+        {// render.material.mainTextureScale = new Vector2(transform.localScale.x , transform.localScale.y);
+            render.material.mainTextureScale = new Vector2(transform.localScale.x / transform.localScale.y, render.material.mainTextureScale.y);
+        }
+
+    }
+
+}
