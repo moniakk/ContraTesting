49d5bbf71866712f8b912d86a659a5d6a39242ae
moniakk <nroshkius@gmail.com>
2015-11-17 00:00:35 +0400

    Fish

---
+++ Assets/Resources/Fish/Fish1/Script/Fish1.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fish1 : MonoBehaviour {
+    float MaxDistance = 5f;
+    float Speed = 1f;
+    float _Speed;
+    bool IsStarted;
+
+    bool Agresive = true;
+    Vector3 NewPosition;
+    Vector3 StartPosition;
+    bool LastPoint = true;
+    
+
+    void Start() {
+        StartPosition = transform.position;
+        _Speed = Speed;
+    }
+
+
+    void Update() {
+        if (IsStarted) {
+            if (LastPoint) {
+                NewPosition = StartPosition + new Vector3(Random.Range(-MaxDistance, MaxDistance), Random.Range(-MaxDistance, MaxDistance), 0);
+                LastPoint = false;
+            }
+
+
+
+            var dir = NewPosition - transform.position;
+
+
+            var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+            var newRotation = Quaternion.AngleAxis(angle, Vector3.forward);
+
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, Time.deltaTime * 1 * _Speed);
+
+
+            transform.Translate(Vector3.right * _Speed * Time.deltaTime);
+
+            if (Vector3.Distance(NewPosition, transform.position) < 1) {
+                LastPoint = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D Hit) {
+        if (Hit.tag == "Water") {
+            _Speed = Speed ;
+        }
+    }
+    void OnTriggerExit2D(Collider2D Hit) {
+        if (Hit.tag == "Water") {
+            NewPosition = StartPosition;
+            _Speed = Speed * 5;
+
+        }
+    }
+
+    void OnBecameInvisible() {
+        IsStarted = false;
+    }
+    void OnBecameVisible() {
+        IsStarted = true;
+    }
+}
---
+++ Assets/Resources/Water/Water3/Script/WaterDetector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WaterDetector : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D Hit) {
+        if (Hit.tag == "Fish") {
+            transform.parent.GetComponent<Water3>().Splash(transform.position.x, .1f);
+        }
+        if (Hit.transform.GetComponent<Rigidbody2D>() != null) {
+            transform.parent.GetComponent<Water3>().Splash(transform.position.x, Hit.transform.GetComponent<Rigidbody2D>().velocity.y * Hit.transform.GetComponent<Rigidbody2D>().mass / 40f);
+        }
+    }
+
+    /*void OnTriggerStay2D(Collider2D Hit)
+    {
+        //print(Hit.name);
+        if (Hit.rigidbody2D != null)
+        {
+            int points = Mathf.RoundToInt(Hit.transform.localScale.x * 15f);
+            for (int i = 0; i < points; i++)
+            {
+                transform.parent.GetComponent<Water>().Splish(Hit.transform.position.x - Hit.transform.localScale.x + i * 2 * Hit.transform.localScale.x / points, Hit.rigidbody2D.mass * Hit.rigidbody2D.velocity.x / 10f / points * 2f);
+            }
+        }
+    }*/
+
+}
