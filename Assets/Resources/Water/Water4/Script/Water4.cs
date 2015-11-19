7c26800bc96e2b0769aeca2fd960d9d66dc2a20b
moniakk <nroshkius@gmail.com>
2015-11-19 23:23:05 +0400

    fire

--- Assets/Resources/Water/Water4/Script/Water4.cs
+++ Assets/Resources/Water/Water4/Script/Water4.cs
@@ -4,12 +4,24 @@ using System.Collections;
 public class Water4 : MonoBehaviour {
 
     public float Speed = .1f;
+    Renderer render;
     void Start() {
 
     }
 
     // Update is called once per frame
     void Update() {
+
+    }
+
+
+    void OnDrawGizmos() {
+        render = GetComponent<Renderer>();
+
+        if (render.material != null) {// render.material.mainTextureScale = new Vector2(transform.localScale.x , transform.localScale.y);
+                                      // render.material.mainTextureScale = new Vector2((transform.localScale.x / 60) * (1 / 12), 1);
+           render.material.mainTextureScale = new Vector2((transform.localScale.x / 60) * (12.2f/ transform.localScale.y), 1);
+        }
 
     }
 }
